e92fc48207cb26030d1b074b3144dc825e7c52ae
aditya0402-debug <119129594+aditya0402-debug@users.noreply.github.com>
2023-07-09 01:48:41 +0530

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -1,6 +1,4 @@
 
-
-//Variables (BE CAREFUL THESE MIGHT BE USED IN OTHER JS FILES TOO)
 var inp_as=document.getElementById('a_size'),array_size=inp_as.value;
 var inp_gen=document.getElementById("a_generate");
 var inp_aspeed=document.getElementById("a_speed");
